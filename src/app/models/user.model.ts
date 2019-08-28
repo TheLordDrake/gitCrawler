fc464dbfb04f47addc7d7a83dfa83b69c17a7a55
Iain Forsgren <jsparn@gmail.com>
2019-08-28 15:57:05 -0400

    Improved handling of user data (#7)
    * Removed unnecessary code.
    
    * Added link to user's git profile. Added link to user's website. Toggled user details off if not populated.

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -5,15 +5,16 @@ export class User {
     login: string;
     name: string;
     email: string;
+    blog: string;
     location: string;
     avatarUrl: string;
     gravatarId: string;
     bio: string;
     company: string;
     hireable: boolean;
+    htmlUrl: string;
 
     url: string;
-    htmlUrl: string;
     reposUrl: string;
     publicRepos: number;
     publicGists: number;
@@ -29,7 +30,6 @@ export class User {
     organizationsUrl: string;
     eventsUrl: string;
     receivedEventsUrl: string;
-    blog: string;
 
 
     constructor(raw?: RawUser) {
@@ -37,10 +37,12 @@ export class User {
         this.login = raw.login;
         this.name = raw.name;
         this.email = raw.email;
+        this.blog = raw.blog;
         this.location = raw.location;
         this.avatarUrl = raw.avatar_url;
         this.bio = raw.bio;
         this.company = raw.company;
         this.hireable = raw.hireable;
+        this.htmlUrl = raw.html_url;
     }
 }
